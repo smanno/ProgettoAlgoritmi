8e76bb1a008dc7fea9ba85ed725bcc696ad7645a
stefano <s.manno@live.it>
2019-12-23 19:18:21 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/ProgettoAlgoritmi.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProgettoAlgoritmi.dir/main.cpp.obj"
+  "ProgettoAlgoritmi.exe"
+  "ProgettoAlgoritmi.exe.manifest"
+  "ProgettoAlgoritmi.pdb"
+  "libProgettoAlgoritmi.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProgettoAlgoritmi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
